e4e6a812e0e92423b1f535ad534f5b679e38323a
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-24 22:56:25 +0300

    feat: enable API tokens on users (#10)

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -14,6 +16,16 @@ class User extends Authenticatable
     protected $fillable = ['name', 'email', 'password', 'locale', 'is_active'];
 
     protected $hidden = ['password', 'remember_token'];
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class, 'role_user');
+    }
+
+    public function storeRoleAssignments(): HasMany
+    {
+        return $this->hasMany(UserStoreRole::class);
+    }
 
     protected function casts(): array
     {
