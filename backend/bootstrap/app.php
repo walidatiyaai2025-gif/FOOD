e9ce3cea08d00935d17dbb42a1932d41c0ed504c
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-24 22:59:00 +0300

    feat: register active auth middleware (#10)

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureActiveUser;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/health',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'active.user' => EnsureActiveUser::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
