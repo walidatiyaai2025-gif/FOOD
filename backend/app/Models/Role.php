f544f4a6b835148c422bbc8c25fbd4ee0b5df942
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-24 23:03:16 +0300

    feat: enforce role permissions server-side (#11)

--- backend/app/Models/Role.php
+++ backend/app/Models/Role.php
@@ -3,10 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
     protected $table = 'roles';
 
     protected $guarded = [];
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class, 'permission_role');
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'role_user');
+    }
 }
