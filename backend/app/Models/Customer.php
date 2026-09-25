6448c115e0b986299907f1c171c013e797d91dbf
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-25 03:07:03 +0300

    Fix Pint formatting for B2B models (#56)

--- backend/app/Models/Customer.php
+++ backend/app/Models/Customer.php
@@ -11,6 +11,7 @@ class Customer extends Model
     protected $table = 'customers';
 
     protected $guarded = [];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
