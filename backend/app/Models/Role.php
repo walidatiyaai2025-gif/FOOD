ff31359404a259581d0adafb9ab6da4193b38964
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-25 14:02:45 +0300

    Add typed role relations for static analysis (#96)

--- backend/app/Models/Role.php
+++ backend/app/Models/Role.php
@@ -6,22 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property int $id
+ * @property string $code
+ * @property string $name
+ * @property string|null $description
+ * @property string $scope
+ * @property bool $is_system
+ * @property bool $is_active
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, Permission> $permissions
+ */
 class Role extends Model
 {
     protected $table = 'roles';
 
     protected $guarded = [];
 
+    /** @return BelongsToMany<Permission, $this> */
     public function permissions(): BelongsToMany
     {
         return $this->belongsToMany(Permission::class, 'permission_role');
     }
 
+    /** @return BelongsToMany<User, $this> */
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'role_user');
     }
 
+    /** @return HasMany<UserStoreRole, $this> */
     public function storeAssignments(): HasMany
     {
         return $this->hasMany(UserStoreRole::class);
