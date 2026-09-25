45d0e0f9ace511e439ece8d6ea8cf3c2e7fb51c6
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-25 09:39:20 +0300

    Scroll B2B cart before checkout assertion (#66)

--- apps/customer_app/test/b2b_journey_test.dart
+++ apps/customer_app/test/b2b_journey_test.dart
@@ -31,8 +31,9 @@ void main() {
 
     await tester.pumpWidget(const FoodexCustomerApp(session: b2b, initialRoute: '/b2b/cart', b2bApi: _StaticB2bApi()));
     await tester.pumpAndSettle();
-    final checkoutButton = find.text('إتمام الطلب');
-    expect(checkoutButton, findsOneWidget);
+    await tester.drag(find.byType(ListView), const Offset(0, -300));
+    await tester.pumpAndSettle();
+    expect(find.text('إتمام الطلب'), findsOneWidget);
   });
 
   testWidgets('B2C session cannot enter B2B protected journey', (tester) async {
