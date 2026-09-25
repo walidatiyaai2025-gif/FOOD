c5b8b6470dec5c6aca7d50e3a25d585378787a56
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-25 03:27:59 +0300

    feat: add customer favorite model

---
+++ backend/app/Models/CustomerFavorite.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerFavorite extends Model
+{
+    protected $table = 'customer_favorites';
+
+    protected $guarded = [];
+}
