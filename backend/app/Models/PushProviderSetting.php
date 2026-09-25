4dc24c168be6c509f8c2197f21359bd1ef9e25d3
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-25 14:29:18 +0300

    Add mobile runtime and secure push foundations (#98)

---
+++ backend/app/Models/PushProviderSetting.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PushProviderSetting extends Model
+{
+    protected $guarded = [];
+    protected $hidden = ['credentials_encrypted'];
+
+    protected function casts(): array
+    {
+        return ['enabled' => 'boolean', 'credentials_encrypted' => 'encrypted'];
+    }
+}
