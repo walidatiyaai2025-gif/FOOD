1c861e524be924bc11634e394584ffa97c2c5977
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-25 09:29:30 +0300

    Fix B2B journey Flutter finder API (#66)

--- apps/customer_app/test/b2b_journey_test.dart
+++ apps/customer_app/test/b2b_journey_test.dart
@@ -31,7 +31,7 @@ void main() {
 
     await tester.pumpWidget(const FoodexCustomerApp(session: b2b, initialRoute: '/b2b/cart', b2bApi: _StaticB2bApi()));
     await tester.pumpAndSettle();
-    expect(find.text('إتمام الطلب'), findsOneWidget, skipOffstage: false);
+    expect(find.text('إتمام الطلب', skipOffstage: false), findsOneWidget);
   });
 
   testWidgets('B2C session cannot enter B2B protected journey', (tester) async {
