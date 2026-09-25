80fd447ed71b209e64a8b8a7f7fc74881b3f9a31
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-25 10:34:43 +0300

    Wire Translation Center routes and permissions (#91)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\Api\V1\HealthController;
 use App\Http\Controllers\Api\V1\InventoryController;
 use App\Http\Controllers\Api\V1\OrderController;
 use App\Http\Controllers\Api\V1\TranslationController;
+use App\Http\Controllers\Api\V1\TranslationController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function (): void {
@@ -28,6 +29,7 @@ Route::prefix('v1')->group(function (): void {
     ]));
 
     Route::get('/app-version', AppVersionController::class);
+    Route::get('/translations/{locale}', TranslationController::class)->whereIn('locale', ['ar', 'en']);
     Route::get('/translations/{locale}', TranslationController::class)->whereIn('locale', ['ar', 'en']);
 
     Route::post('/auth/login', [AuthController::class, 'login'])
