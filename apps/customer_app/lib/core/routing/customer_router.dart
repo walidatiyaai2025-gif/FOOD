036db779a841a6124b0f2558cb095c9b7ff3bcae
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-26 05:06:50 +0300

    feat(customer): route B2C account runtime API

--- apps/customer_app/lib/core/routing/customer_router.dart
+++ apps/customer_app/lib/core/routing/customer_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../api/b2b_api.dart';
 import '../api/b2c_catalog_api.dart';
+import '../api/b2c_account_api.dart';
 import '../api/customer_action_api.dart';
 import '../auth/customer_session.dart';
 import '../../features/b2b/b2b_journey_screen.dart';
@@ -16,11 +17,13 @@ class CustomerAppRouter {
     required this.onAuthenticated,
     this.b2bApi,
     required this.b2cCatalogApi,
+    required this.b2cAccountApi,
   });
 
   final CustomerSession session;
   final B2bApi? b2bApi;
   final B2cCatalogApi b2cCatalogApi;
+  final B2cAccountApi b2cAccountApi;
   final CustomerActionApi actionApi;
   final CustomerAuthenticated onAuthenticated;
 
@@ -105,6 +108,7 @@ class CustomerAppRouter {
               location: requestedLocation,
               actionApi: actionApi,
               catalogApi: b2cCatalogApi,
+              accountApi: b2cAccountApi,
               onAuthenticated: onAuthenticated,
             ),
     );
