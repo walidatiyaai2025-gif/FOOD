78274e6617a394407626cb21d899fe6012d76069
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-26 17:30:04 +0300

    feat(push): bootstrap Firebase messaging

--- apps/driver_app/lib/main.dart
+++ apps/driver_app/lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/widgets.dart';
-import 'app.dart';
 
-void main() => runApp(const FoodexDriverApp());
+import 'app.dart';
+import 'core/push/firebase_push_service.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final pushService = await DriverFirebasePushService.bootstrap();
+  runApp(FoodexDriverApp(pushService: pushService));
+}
