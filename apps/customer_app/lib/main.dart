19bf7d1bd62b36795cca296f974cd7c1dc54fe02
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-26 17:30:01 +0300

    feat(push): bootstrap Firebase messaging

--- apps/customer_app/lib/main.dart
+++ apps/customer_app/lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/widgets.dart';
-import 'app.dart';
 
-void main() => runApp(const FoodexCustomerApp());
+import 'app.dart';
+import 'core/push/firebase_push_service.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final pushService = await CustomerFirebasePushService.bootstrap();
+  runApp(FoodexCustomerApp(pushService: pushService));
+}
